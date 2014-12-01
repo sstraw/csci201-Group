da80b0dde1a881bf08791ff8f463c088b4bb98ce
Sean Straw <straw@usc.edu>
2014-11-30 17:58:53 -0800

    Dashboard 2-1 updated to generic format

--- csci201-Group-client/src/client/Dashboard2_1.java
+++ csci201-Group-client/src/client/Dashboard2_1.java
@@ -1,5 +1,6 @@
 package client;
 import java.awt.*;
+import java.util.Arrays;
 import java.util.Vector;
 
 import javax.swing.*;
@@ -131,9 +132,9 @@ public class Dashboard2_1 implements Dashboard
 		
 		widgets = new Vector<Widget>(5);
 		widgets.add(new Slider("Finite Plexus", 0, 4, 3));
-		widgets.add(new TurboJumper(0));
-		widgets.add(new Leftovers());
-		widgets.add(new IonTissue(0));
+		widgets.add(new AnyButton("Turbo Jumper", 2, 0, new Vector<String>(Arrays.asList("Set Turbo Jumper to 0", "Set TurboJumper to 1"))));
+		widgets.add(new AnyButton("Leftovers", 1, 0, new Vector<String>(Arrays.asList("Freeze Leftovers"))));
+		widgets.add(new AnyButton("Ion Tissue", 3, 0, new Vector<String>(Arrays.asList("Engorge Ion Tissue", "Wipe Ion Tissue", "Uncork Ion Tissue"))));
 		widgets.add(new Slider("Finite Moonbeam",0, 4, 0));
 	}
 
