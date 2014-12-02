15588fcad45d42db5be0cc63d621a3a19627d8ea
jofuente <jofuente@usc.edu>
2014-12-01 16:33:28 -0800

    Fixed scroll FOR REAL THIS TIME

--- csci201-Group-client/src/client/ClientGUI.java
+++ csci201-Group-client/src/client/ClientGUI.java
@@ -167,7 +167,7 @@ public class ClientGUI extends JFrame implements Serializable {
 		groupChat.setFocusable(false);
 		groupPanelChat.add(groupChat);
 		JScrollPane groupChatScrollPane = new JScrollPane();
-		groupChatScrollPane.getViewport().add(groupChat);
+		groupChatScrollPane.getViewport().add(groupPanelChat);
 		groupChatScrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
 		groupChatScrollPane.setFocusable(false);
 		
